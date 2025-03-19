6aaef62d978f13ea81ce4ed979a48fc4c23d221c
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-03-19 12:29:55 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=BloodReaper+ Kernel by CuriousNom
+kernel.string=Flashing kernel images for gta4l series
 do.devicecheck=1
 do.modules=0
 do.systemless=1
