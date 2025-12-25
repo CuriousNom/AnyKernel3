fb624a6cc9bb2baf1e322e270a02ee70514f44a2
CuriousNom <aryanbiswas7585@gmail.com>
2025-12-25 22:09:50 +0530

    ak3: Flash dtbo image
    Change-Id: I8ab07d90762f387fe664b2549da859ee6e27c705

--- anykernel.sh
+++ anykernel.sh
@@ -47,10 +47,14 @@ ui_print ""
 ## Prepare Kernel and DTB
 mv "$home/kernels/Image" "$home/Image"
 mv "$home/kernels/dtb" "$home/dtb"
+mv "$home/kernels/dtbo.img" "$home/dtbo.img"
 
 ## Boot Partition Flash
 split_boot
 flash_boot
+
+## Flash DTBO
+flash_dtbo
 
 ## Vendor Boot Partition Flash
 block=vendor_boot
